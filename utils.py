ad2c39e8b53bc504b1ae67da68ca598d5bf3706c
Jarana Manotumruksa <feay1234@gmail.com>
2019-09-02 17:29:39 +0100

    data pre, on progress

--- utils.py
+++ utils.py
@@ -4,21 +4,24 @@ import os
 from keras.initializers import Constant
 from keras.layers import Embedding
 from keras.preprocessing.text import Tokenizer
+from keras_preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
 
 
-def get_pretrain_embeddings(path, MAX_NUM_WORDS, EMBEDDING_DIM, MAX_SEQUENCE_LENGTH, word_index):
+def get_pretrain_embeddings(path, MAX_NUM_WORDS, word_index, EMBEDDING_DIM=300):
     BASE_DIR = path + 'data/'
     GLOVE_DIR = os.path.join(BASE_DIR, 'w2v')
     print('Indexing word vectors.')
 
     embeddings_index = {}
-    with open(os.path.join(GLOVE_DIR, 'glove.840B.300d.txt'), encoding="utf-8") as f:
+    with open(os.path.join(GLOVE_DIR, 'glove.42B.300d.txt'), encoding="utf-8") as f:
         for line in f:
             word, coefs = line.split(maxsplit=1)
             coefs = np.fromstring(coefs, 'f', sep=' ')
             embeddings_index[word] = coefs
-            # break
+            break
     #
+
     print('Found %s word vectors.' % len(embeddings_index))
 
     print('Preparing embedding matrix.')
@@ -48,6 +51,12 @@ def get_pretrain_embeddings(path, MAX_NUM_WORDS, EMBEDDING_DIM, MAX_SEQUENCE_LEN
     return embedding_layer
 
 
+def tagger(decoder_input_sentence, tag):
+    if tag == "<EOS>":
+        final_target = [text + tag for text in decoder_input_sentence]
+    elif tag == "<SOS>":
+        final_target = [tag + text for text in decoder_input_sentence]
+    return final_target
 
 
 def save2file(path, output):
@@ -56,17 +65,21 @@ def save2file(path, output):
     print(output)
 
 
-def getAOL(dir="data/tmp/", MAX_NUM_WORDS=50000, MAX_SEQUENCE_LENGTH=50):
+def getAOL(dir="data/tmp/", MAX_NUM_WORDS=70000, MAX_SEQUENCE_LENGTH=50):
 
-    q1, q2 = [], []
-    for i in os.listdir(dir):
+    encoder_inputs, decoder_inputs = [], []
+    for i in os.listdir(dir)[:100]:
         f = open("%s%s" % (dir, i), "r")
         q = f.readline().strip().split("\t")
         for i in range(len(q) - 1):
-            q1.append(q[i])
-            q2.append(q[i + 1])
+            encoder_inputs.append(q[i])
+            decoder_inputs.append(q[i + 1])
 
-    corpus = q1 + q2
+    # add EOS and SOS into decoder
+    decoder_inputs = tagger(decoder_inputs)
+    print(decoder_inputs)
+
+    corpus = encoder_inputs + decoder_inputs
 
     # finally, vectorize the text samples into a 2D integer tensor
     tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
@@ -76,12 +89,20 @@ def getAOL(dir="data/tmp/", MAX_NUM_WORDS=50000, MAX_SEQUENCE_LENGTH=50):
     print('Found %s unique tokens.' % len(word_index))
 
     # Update max length
+    print(np.max([len(i) for i in corpus]))
     MAX_SEQUENCE_LENGTH = min(np.max([len(i) for i in corpus]), MAX_SEQUENCE_LENGTH)
     print("Updated maxlen: %d" % MAX_SEQUENCE_LENGTH)
 
-    x1 = tokenizer.texts_to_sequences(q1)
-    x2 = tokenizer.texts_to_sequences(q2)
+    encoder_inputs = pad_sequences(tokenizer.texts_to_sequences(encoder_inputs), maxlen=MAX_SEQUENCE_LENGTH)
+    decoder_inputs = pad_sequences(tokenizer.texts_to_sequences(decoder_inputs), maxlen=MAX_SEQUENCE_LENGTH)
 
-    return x1, x2
+    x_train, x_test, y_train, y_test = train_test_split(encoder_inputs, decoder_inputs, test_size=0.33, random_state=2019)
 
-data = getAOL()
+    # decoder_input_data =
+
+
+    return x_train, x_test, y_train, y_test, word_index, len(word_index), MAX_SEQUENCE_LENGTH
+
+# data = getAOL()
+# print(data[0])
+# print(data[2])
