ea8140f689a64ca4fed8e282e9c70c3a58076471
Jarana Manotumruksa <feay1234@gmail.com>
2019-09-10 14:54:16 +0100

    add lstm

--- Models.py
+++ Models.py
@@ -1,14 +1,17 @@
 from keras.models import Sequential
 from keras import Input, Model
-from keras.layers import GlobalMaxPooling1D, Dot
+from keras.layers import GlobalMaxPooling1D, Dot, LSTM
 
 
 def getDSSM(embedding_layer, MAX_SEQUENCE_LENGTH):
 
     q_inp = Input(shape=(MAX_SEQUENCE_LENGTH,))
     t_inp = Input(shape=(MAX_SEQUENCE_LENGTH,))
-    q_emb = GlobalMaxPooling1D()(embedding_layer(q_inp))
-    t_emb = GlobalMaxPooling1D()(embedding_layer(t_inp))
+    lstm = LSTM(100)
+    # q_emb = GlobalMaxPooling1D()(embedding_layer(q_inp))
+    # t_emb = GlobalMaxPooling1D()(embedding_layer(t_inp))
+    q_emb = lstm(embedding_layer(q_inp))
+    t_emb = lstm(embedding_layer(t_inp))
     pred = Dot(-1, normalize=True)([q_emb, t_emb])
     model = Model([q_inp, t_inp], pred)
 
