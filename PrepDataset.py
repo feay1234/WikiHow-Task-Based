bb83739069d94ab9b93d7cb2e4fb7013785cf8d1
Jarana Manotumruksa <feay1234@gmail.com>
2019-09-12 12:39:52 +0100

    finish ranking task

--- PrepDataset.py
+++ PrepDataset.py
@@ -1,21 +1,58 @@
-
 import pandas as pd
 import numpy as np
 import random
+from string import digits
+import re
 
-df = pd.read_csv("data/d3_wikihow.csv")
-task_nb = df.Task.unique()
-for idx, row in df[df.Source == "google"].iterrows():
-    pos_task = row.Task
 
-    # random negative task
-    neg_task = random.choice(task_nb)
-    while neg_task == pos_task:
+def query_task_prediction():
+    df = pd.read_csv("data/d3_wikihow.csv")
+    task_nb = df.Task.unique()
+    for idx, row in df[df.Source == "google"].iterrows():
+        pos_task = row.Task
+
+        # random negative task
         neg_task = random.choice(task_nb)
+        while neg_task == pos_task:
+            neg_task = random.choice(task_nb)
 
-    pos_article = df[(df.Task == pos_task) & (df.Source == "wikihow")].sample(1).Query.values[0]
-    neg_article = df[(df.Task == neg_task) & (df.Source == "wikihow")].sample(1).Query.values[0]
+        pos_article = df[(df.Task == pos_task) & (df.Source == "wikihow")].sample(1).Query.values[0]
+        neg_article = df[(df.Task == neg_task) & (df.Source == "wikihow")].sample(1).Query.values[0]
 
-    with open('data/query_task_prediction2.csv', 'a') as the_file:
-        the_file.write('%s' % row.Query.strip() + "\t" + pos_article + "\t1\n")
-        the_file.write('%s' % row.Query.strip() + "\t" + neg_article + "\t0\n")
+        with open('data/query_task_prediction2.csv', 'a') as the_file:
+            the_file.write('%s' % row.Query.strip() + "\t" + pos_article + "\t1\n")
+            the_file.write('%s' % row.Query.strip() + "\t" + neg_article + "\t0\n")
+
+def step_prediction():
+    wiki = pd.read_csv("data/wikihowSep.csv")
+    wiki = wiki.dropna()
+    wiki['headline'] = [i.strip() for i in wiki['headline']]
+
+    remove_digits = str.maketrans('', '', digits)
+    wiki['title'] = [i.translate(remove_digits) for i in wiki['title']]
+    for col in wiki:
+        wiki[col] = [re.sub(r'[\W_]+', ' ', i).lower() for i in wiki[col]]
+
+    singleWiki = wiki[wiki['sectionLabel'] == "steps"]
+    for name, group in singleWiki.groupby("title"):
+        for i in range(len(group) - 1):
+            with open('data/step_prediction.csv', 'a') as the_file:
+                the_file.write(
+                    group.iloc[i].headline + "\t" + group.iloc[i + 1].headline + "\t" + group.iloc[i].text + "\t" +
+                    group.iloc[i + 1].text + "\t" + name + "\n")
+
+    singleWiki = wiki[wiki['sectionLabel'] == "steps"]
+    tasks = singleWiki.title.unique()
+    for name, group in singleWiki.groupby("title"):
+        for idx, row in group.iterrows():
+            pos_task = row.sectionLabel if row.sectionLabel != "steps" else row.title
+            step = row.headline
+            text = row.text
+
+            neg_task = random.choice(tasks)
+            while neg_task == pos_task:
+                neg_task = random.choice(tasks)
+
+            with open('data/step_task_prediction.csv', 'a') as the_file:
+                the_file.write(step +"\t"+ text + "\t" + pos_task + "\t1\n")
+                the_file.write(step +"\t"+ text + "\t" + neg_task + "\t0\n")
---
+++ run_step_prediction.py
@@ -0,0 +1,56 @@
+import argparse
+
+import pandas as pd
+import numpy as np
+import random
+
+from Models import getDSSM, getRanker
+from utils import *
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Run Task Prediction Experiments")
+
+    parser.add_argument('--path', type=str, help='Path to data', default="/Users/jarana/workspace/WikiHow-Task-Based/")
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+    path = args.path
+
+
+    MAX_NUM_WORDS = 100000
+
+
+    data = pd.read_csv(path+"data/step_prediction.csv", sep="\t", names=["s1", "s2", "s1_text", "s2_text", "task"])
+
+    # corpus = howto.Query.tolist() + df.Query.tolist()
+    corpus = list(set(data.s1.tolist() + data.s2.tolist() + data.task.tolist()))
+    # corpus = [str(i) for i in corpus]
+    # finally, vectorize the text samples into a 2D integer tensor
+    tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+    tokenizer.fit_on_texts(corpus)
+
+    word_index = tokenizer.word_index
+    print('Found %s unique tokens.' % len(word_index))
+
+
+    max_words = len(word_index)
+    embedding_layer = get_pretrain_embeddings(path, word_index)
+
+
+
+    MAX_SEQUENCE_LENGTH = min(max([len(i.split()) for i in corpus]), 200)
+    x_task = pad_sequences(tokenizer.texts_to_sequences(data["task"].tolist()), maxlen=MAX_SEQUENCE_LENGTH)
+    x_step1 = pad_sequences(tokenizer.texts_to_sequences(data["s1"].tolist()), maxlen=MAX_SEQUENCE_LENGTH)
+    x_step2 = pad_sequences(tokenizer.texts_to_sequences(data["s2"].tolist()), maxlen=MAX_SEQUENCE_LENGTH)
+    y = np.zeros((len(x_task), 2))
+
+
+    model = getRanker(embedding_layer, MAX_SEQUENCE_LENGTH)
+
+    model.fit([x_task, x_step1, x_step2], y, batch_size=128, validation_split=0.3, epochs=10, verbose=2)
