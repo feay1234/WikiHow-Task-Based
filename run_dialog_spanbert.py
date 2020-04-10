7c2d29154891ab6da6d5cbe667de7a4b455626e0
Jarana Manotumruksa <feay1234@gmail.com>
2020-04-10 11:52:49 +0100

    more input arg and save res to file

--- run_dialog_spanbert.py
+++ run_dialog_spanbert.py
@@ -2,7 +2,15 @@ import argparse
 
 from simpletransformers.question_answering import QuestionAnsweringModel
 import logging
-import torch
+import torch, os
+
+def print2file(path, name, format, printout):
+    print(printout)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    thefile = open(path + name + format, 'a')
+    thefile.write("%s\n" % (printout))
+    thefile.close()
 
 logging.basicConfig(level=logging.INFO)
 transformers_logger = logging.getLogger("transformers")
@@ -11,39 +19,34 @@ transformers_logger.setLevel(logging.WARNING)
 parser = argparse.ArgumentParser('Dialog System with Transformers')
 parser.add_argument('--model', type=str)
 parser.add_argument('--pre', type=str, default="")
+parser.add_argument('--train', type=str)
+parser.add_argument('--test', type=str)
+parser.add_argument('--out_dir', type=str, default="data/dialog/out/")
 args = parser.parse_args()
 
 pretrain = {'albert':'albert-xlarge-v2', 'bert':'bert-large-uncased', 'distilbert':'distilbert-base-uncased'}
 pretrainSQUAD = {'albert':'ktrapeznikov/albert-xlarge-v2-squad-v2', 'bert':'bert-large-uncased-whole-word-masking-finetuned-squad', 'distilbert':'distilbert-base-uncased-distilled-squad'}
 pretrainFile = pretrain[args.model] if args.pre == "" else pretrainSQUAD[args.model]
 
+modelName = "%s_%s_%s" % (args.model, pretrainFile, args.train)
+
 
 # Create the QuestionAnsweringModel
 model = QuestionAnsweringModel(args.model, pretrainFile, use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
 
-# model = QuestionAnsweringModel('albert', 'albert-xlarge-v2', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
-# model = QuestionAnsweringModel('albert', 'ktrapeznikov/albert-xlarge-v2-squad-v2', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
-# model = QuestionAnsweringModel('distilbert', 'distilbert-base-uncased', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
-# model = QuestionAnsweringModel('distilbert', 'distilbert-base-uncased-distilled-squad', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
-# model = QuestionAnsweringModel('bert', 'bert-large-uncased', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
-# model = QuestionAnsweringModel('bert', 'bert-large-uncased-whole-word-masking-finetuned-squad', use_cuda=torch.cuda.is_available(), args={'reprocess_input_data': True, 'overwrite_output_dir': True})
+# Train the model with JSON file
+model.train_model('data/dialog/%s.json' % args.train)
 
+# Evaluate the model. (Being lazy and evaluating on the train data itself)
+result, text = model.eval_model('data/dialog/%s.json' % args.test)
 
-# # Train the model with JSON file
-# model.train_model('data/dialog/train.json')
+print(result)
+print(text)
+print2file(args.out_dir, modelName, ".res", result)
+
+print('-------------------')
+
+# Making predictions using the model.
+# to_predict = [{'context': 'This is the context used for demonstrating predictions.', 'qas': [{'question': 'What is this context?', 'id': '0'}]}]
 #
-# # The list can also be used directly
-# # model.train_model(train_data)
-#
-# # Evaluate the model. (Being lazy and evaluating on the train data itself)
-# result, text = model.eval_model('data/dialog/test.json')
-#
-# print(result)
-# print(text)
-#
-# print('-------------------')
-#
-# # Making predictions using the model.
-# # to_predict = [{'context': 'This is the context used for demonstrating predictions.', 'qas': [{'question': 'What is this context?', 'id': '0'}]}]
-# #
-# # print(model.predict(to_predict))
+# print(model.predict(to_predict))
